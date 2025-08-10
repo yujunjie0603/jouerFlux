c956f6ff00eaad1926e7c3afa023c03dc333b19e
yujunjie0603 <yu.long.fr@gmail.com>
2025-08-10 19:43:46 +0200

    add run file

---
+++ run.py
@@ -0,0 +1,6 @@
+"This script is the entry point for running the Flask application."
+from app import create_app
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', use_reloader=True)
